4b4ebd56aa5d74e907522a879f485bcba892c44d
EstebanGS13 <49171835+esteban-gasan@users.noreply.github.com>
2021-10-11 18:19:31 -0500

    Update load_mailbox and create a view for a single email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -17,10 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("#compose-form").onsubmit = send_email;
 });
 
+function display_view(view) {
+  // Hide all the views, then only display the requested view
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#single-view").style.display = "none";
+  document.querySelector("#compose-view").style.display = "none";
+
+  document.querySelector(`#${view}-view`).style.display = "block";
+}
+
 function compose_email() {
   // Show compose view and hide other views
-  document.querySelector("#emails-view").style.display = "none";
-  document.querySelector("#compose-view").style.display = "block";
+  display_view("compose");
 
   // Clear out composition fields
   document.querySelector("#compose-recipients").value = "";
@@ -29,15 +37,60 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
+  fetch(`/emails/${mailbox}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((email) => {
+        // Dynamically create a div for each email
+        const div = document.createElement("div");
+        const baseClass = "email-box";
+        // Add class for gray background if email is read
+        const bootstrapClasses = `list-group-item list-group-item-action${
+          email.read ? " list-group-item-dark" : ""
+        }`;
+
+        div.className = `${baseClass} ${bootstrapClasses}`;
+        div.dataset.id = `${email.id}`;
+        div.innerHTML = `<p>Sender: ${email.sender}</p><p>Subject: ${email.subject}</p><p>${email.timestamp}</p>`;
+        div.addEventListener("click", open_email);
+        document.querySelector("#emails-view").append(div);
+      });
+    });
 
   // Show the mailbox and hide other views
-  document.querySelector("#emails-view").style.display = "block";
-  document.querySelector("#compose-view").style.display = "none";
+  display_view("emails");
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
+}
+
+function open_email() {
+  // Display an email and its details
+  display_view("single");
+  fetch(`/emails/${this.dataset.id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      // Update the single view with the email data
+      document.querySelector("#email-sender").innerHTML = email.sender;
+      document.querySelector("#email-recipients").innerHTML =
+        email.recipients.join(", ");
+      document.querySelector("#email-timestamp").innerHTML = email.timestamp;
+      document.querySelector("#email-body").innerHTML = email.body;
+      console.log(email);
+
+      if (email.read === false) {
+        // Modify the read property of this mail on the server
+        fetch(`/emails/${this.dataset.id}`, {
+          method: "PUT",
+          body: JSON.stringify({
+            read: true,
+          }),
+        });
+      }
+    });
 }
 
 function send_email() {
@@ -47,11 +100,10 @@ function send_email() {
     body: document.querySelector("#compose-body").value,
   };
 
-  console.log(email_data);
   fetch("/emails", {
-      method: "POST",
-      body: JSON.stringify(email_data),
-    })
+    method: "POST",
+    body: JSON.stringify(email_data),
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
